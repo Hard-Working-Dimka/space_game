de5f0c19c09c049b2ccc8be6449183c200947a0d
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-06-11 00:45:37 +0300

    init

---
+++ game.py
@@ -0,0 +1,60 @@
+import time
+import curses
+import asyncio
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
+def draw(canvas):
+    canvas.border('|', '|')
+    curses.curs_set(False)
+
+    row, column = (5, 20)
+    coroutine = blink(canvas, row, column)
+
+    while True:
+        try:
+            coroutine.send(None)
+            canvas.refresh()
+            time.sleep(1)
+        except StopIteration: #TODO: delete
+            break
+
+    # while True:
+    #     row, column = (5, 20)
+    #     canvas.border('|', '|')
+    #     curses.curs_set(False)
+    #
+    #     canvas.addstr(row, column, '*', curses.A_DIM)
+    #     canvas.refresh()
+    #     time.sleep(2)
+    #
+    #     canvas.addstr(row, column, '*')
+    #     canvas.refresh()
+    #     time.sleep(0.3)
+    #
+    #     canvas.addstr(row, column, '*', curses.A_BOLD)
+    #     canvas.refresh()
+    #     time.sleep(0.5)
+    #
+    #     canvas.addstr(row, column, '*')
+    #     canvas.refresh()
+    #     time.sleep(0.3)
+
+
+if __name__ == '__main__':
+    curses.update_lines_cols()
+    curses.wrapper(draw)
