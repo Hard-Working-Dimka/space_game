7f32565edd75afe0a8e285c8d4b033394143b0fe
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-06-11 20:17:41 +0300

    add random stars

--- game.py
+++ game.py
@@ -1,60 +1,61 @@
+import random
 import time
 import curses
 import asyncio
 
 
+TIC_TIMEOUT = 0.1
+MAX_STARS = 30
+MIN_STARS = 15
+ICONS_OF_STARS = ['+', '*', '.', ':']
+
+
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        for _ in range(20):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        for _ in range(5):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
 
 def draw(canvas):
     canvas.border('|', '|')
     curses.curs_set(False)
 
-    row, column = (5, 20)
-    coroutine = blink(canvas, row, column)
+    rows, columns = canvas.getmaxyx()
+    quantity_of_stairs = random.randint(MIN_STARS, MAX_STARS)
+
+    coroutines = []
+    for _ in range(quantity_of_stairs):
+        row = random.randint(1, rows - 2)
+        column = random.randint(1, columns - 2)
+        coroutines.append(blink(canvas, row, column, symbol=random.choice(ICONS_OF_STARS)))
 
     while True:
-        try:
-            coroutine.send(None)
-            canvas.refresh()
-            time.sleep(1)
-        except StopIteration: #TODO: delete
-            break
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
 
-    # while True:
-    #     row, column = (5, 20)
-    #     canvas.border('|', '|')
-    #     curses.curs_set(False)
-    #
-    #     canvas.addstr(row, column, '*', curses.A_DIM)
-    #     canvas.refresh()
-    #     time.sleep(2)
-    #
-    #     canvas.addstr(row, column, '*')
-    #     canvas.refresh()
-    #     time.sleep(0.3)
-    #
-    #     canvas.addstr(row, column, '*', curses.A_BOLD)
-    #     canvas.refresh()
-    #     time.sleep(0.5)
-    #
-    #     canvas.addstr(row, column, '*')
-    #     canvas.refresh()
-    #     time.sleep(0.3)
+        canvas.refresh()
+        time.sleep(TIC_TIMEOUT)
+        if len(coroutines) == 0:
+            break
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
+
