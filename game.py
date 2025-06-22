8072a7fab0ffb8baaaa1f0146018327e2ca40ebb
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-06-22 21:23:13 +0300

    add fire on spaceship

--- game.py
+++ game.py
@@ -2,8 +2,11 @@ import time
 import curses
 import asyncio
 import os, random
+from itertools import cycle
 
-from animations.animations import animate_spaceship, fly_garbage
+from animations.animations import fly_garbage, fire
+from animations.physics_of_ship import update_speed
+from curses_tools import get_frame_size, draw_frame, read_controls
 
 TIC_TIMEOUT = 0.1
 MAX_STARS = 30
@@ -30,6 +33,33 @@ async def fill_orbit_with_garbage(canvas, garbage_filenames, columns):
         garbage_filename = random.choice(garbage_filenames)
         garbage_frame = get_frame(f'animations/frames/garbage/{garbage_filename}')
         COROUTINES.append(fly_garbage(canvas, column=random.randint(2, columns - 2), garbage_frame=garbage_frame))
+
+
+async def run_spaceship(canvas, start_row, start_column, *args):
+    rows_canvas, columns_canvas = canvas.getmaxyx()
+    row_speed = column_speed = 0
+
+    for frame in cycle(args):
+        rows_spaceship, columns_spaceship = get_frame_size(frame)
+
+        for _ in range(2):
+            rows_direction, columns_direction, fire_on = read_controls(canvas)
+            row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
+
+            start_row += row_speed
+            start_column += column_speed
+
+            rows_direction_validated = min(max(start_row, 1), rows_canvas - rows_spaceship - 1)
+            columns_spaceship_validated = min(max(start_column, 1), columns_canvas - columns_spaceship - 1)
+
+            start_row, start_column = rows_direction_validated, columns_spaceship_validated
+
+            draw_frame(canvas, start_row, start_column, frame)
+            if fire_on:
+                COROUTINES.append(fire(canvas, start_row, start_column + 2))
+
+            await asyncio.sleep(0)
+            draw_frame(canvas, start_row, start_column, frame, negative=True)
 
 
 async def blink(canvas, row, column, offset_tics, symbol='*'):
@@ -59,8 +89,8 @@ def draw(canvas):
 
     spaceship_first_frame = get_frame('animations/frames/rocket_frame_1.txt')
     spaceship_second_frame = get_frame('animations/frames/rocket_frame_2.txt')
-    COROUTINES.append(animate_spaceship(canvas, 0, columns // 2, spaceship_first_frame,
-                                        spaceship_second_frame))
+    COROUTINES.append(run_spaceship(canvas, 0, columns // 2, spaceship_first_frame,
+                                    spaceship_second_frame))
 
     offset_tics = random.randint(0, OFFSET_OF_ANIMATION)
     for _ in range(quantity_of_stairs):
